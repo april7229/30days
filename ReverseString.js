712720cd5bb1b789a313cadb2b6109e704e1a45d
april <martinezapril56@yahoo.com>
2019-02-07 23:19:06 -0500

    1

---
+++ ReverseString.js
@@ -0,0 +1,30 @@
+function reverseString( str )
+{
+    var strArr = str.split( "" );
+    var reverseStrArray = StrArr.reverse();
+    var reversedString = reverseStrArray.join( "" );
+    return reverseString
+
+}
+console.log( reverseString( "hello" ) );
+
+
+
+function reverseString( str )
+{
+    return str.split( "" ).reverse().join( " " );
+
+}
+
+console.log( reverseString( "hello" ) );
+
+
+function reverseString( str )
+{
+    var final = "";
+    for ( var i = str.length - 1; i >= 0; i-- )
+    {
+        return final;
+    }
+}
+console.log( reversedString( "hello" ) );
